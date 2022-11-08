5dc0eb4711e9b83c4306e09f614e8ca4877f8df7
Tamta Kirtadze <tkirt11@freeuni.edu.ge>
2022-11-08 14:39:31 +0400

    commit updates

--- mqtt_pub.py
+++ mqtt_pub.py
@@ -6,7 +6,7 @@ import paho.mqtt.publish as publish
 from settings import HOSTNAME, USERNAME, PASSWORD, PORT
 
 # create a set of 2 test messages that will be published at the same time
-msgs = [{'topic': "RobotData/battery", 'payload': "87%"}, ("RobotData/position", "[1;1]", 0, False)]
+msgs = [{'topic': "Robot/battery", 'payload': "87%"}, ("RobotData/position", "[1;1]", 0, False)]
 
 # use TLS for secure connection with HiveMQ Cloud
 sslSettings = ssl.SSLContext(mqtt.client.ssl.PROTOCOL_TLS)
